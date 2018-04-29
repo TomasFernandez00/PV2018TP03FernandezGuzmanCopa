1576627ce946973e94da91b07ab850705e631be4
Agustin <Agustin@DESKTOP-AGB4VG3>
2018-04-29 00:02:39 -0300

    Agregado Punto 3

---
+++ src/java/principal03/Principal03.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal03;
+
+import modelo.Triangulo;
+
+/**
+ *
+ * @author Agustin
+ */
+public class Principal03 {
+    public static void main(String[] args) {
+        resPunto3();
+    }
+    public static void resPunto3(){
+        Triangulo unTriangulo=new Triangulo();
+        unTriangulo.calcPer();
+    }
+}
